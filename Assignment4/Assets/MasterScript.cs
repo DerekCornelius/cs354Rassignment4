bfb62d088c079cadbeec5b27ebb17f9dec526d40
David <arumathos@aim.com>
2016-04-20 10:56:41 -0500

    (One) Wall generation example

--- Assignment4/Assets/MasterScript.cs
+++ Assignment4/Assets/MasterScript.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class MasterScript : MonoBehaviour {
 
-	public int floor_size;
+	public int floorSize;
 	public GameObject player;
 	public GameObject[] floor;
+	public GameObject[] wall;
 
 	// Use this for initialization
 	void Start () {
@@ -13,12 +14,25 @@ public class MasterScript : MonoBehaviour {
 		player = (GameObject) Instantiate (player, new Vector3 (0, 1, 0), Quaternion.identity);
 		player.name = "Player";
 
-		//Generate floors
+		//Generate floors and walls
 		GameObject floors = new GameObject();
 		floors.name = "Floor 1";
+
+		GameObject walls = new GameObject();
+		walls.name = "Walls 1";
+		walls.transform.parent = floors.transform;
+
 		float spacing = 10f;
-		for (int x = 0; x < 5; x++) {
-            for (int z = 0; z < 5; z++) {
+		for (int x = 0; x < floorSize; x++) {
+            for (int z = 0; z < floorSize; z++) {
+
+				if (x == 0) {
+					Vector3 rot;
+					//rot = wall [0].transform.rotation;
+					//rot *= Quaternion.Euler (0, 90, 0);
+					GameObject new_wall = (GameObject) Instantiate (wall[0], new Vector3 (spacing * x - (spacing / 2), 0, spacing * z), Quaternion.Euler(0, 90, 0));
+				}
+
 				GameObject new_floor = (GameObject) Instantiate (floor[0], new Vector3 (spacing * x, 0, spacing * z), floor[0].transform.rotation);
 				new_floor.transform.parent = floors.transform;
             }
