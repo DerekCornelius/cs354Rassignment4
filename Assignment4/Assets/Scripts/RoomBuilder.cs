df310143aae4795f8f30349fc035ef081d5c6a04
DerekCornelius <drc2016@gmail.com>
2016-04-27 19:42:20 -0500

    created xbyx room creator I think

--- Assignment4/Assets/Scripts/RoomBuilder.cs
+++ Assignment4/Assets/Scripts/RoomBuilder.cs
@@ -8,10 +8,10 @@ public class RoomBuilder : MonoBehaviour {
 	private int floorSize;
 	private MasterScript m;
 
-	public void buildRoom(r_type buildType, int floor) {
+	public void buildRoom(r_type buildType, int floor, bool debug = false) {
 		switch (buildType) {
 		case (r_type.THREE_BY_THREE):
-			buildThreeByThreeRoom (floor);
+			buildThreeByThreeRoom (floor, debug);
 			break;
 		}
 	}
@@ -34,68 +34,81 @@ public class RoomBuilder : MonoBehaviour {
 
 	}
 
-	private void buildThreeByThreeRoom(int floor) {
+	//This method can only be called explicitly - buildRoom cannot specify the size correctly
+	public void buildXByYRoom(int x, int floor, int z, bool debug = false) {
 
-		Debug.Assert (floorSize > 2);
-		const bool debug = false;
+		int xM = x - 1; //the M stands for minus 1
+		int zM = z - 1;
 
-		int xRandom = Random.Range (0, floorSize - 2);
-		int zRandom = Random.Range (0, floorSize - 2);
+		Debug.Assert (floorSize > xM);
+		Debug.Assert (floorSize > zM);
 
-		/*
+
+		int xRandom = Random.Range (0, floorSize - xM);
+		int zRandom = Random.Range (0, floorSize - zM);
+
 		int attempts = 0;
-		int i = xRandom;
-		int j = zRandom;
-		while (attempts < 5 && ( (i < (3 + xRandom)) && (j < (3 + zRandom)) ) ) {
-			if (m.cells [i, floor, j].cellType == c_type.ROOM) {
-				xRandom = Random.Range (0, floorSize - 2);
-				zRandom = Random.Range (0, floorSize - 2);
-				i = xRandom;
-				j = zRandom;
+		while (attempts < 5) {
+			bool clear = true;
+
+			for (int i = xRandom; i < xRandom + x; i++) {
+				for (int j = zRandom; j < zRandom + z; j++) {
+					if (m.cells [i, floor, j].cellType == c_type.ROOM)
+						clear = false;
+				}
+			}
+			if (!clear) {
 				attempts++;
+				xRandom = Random.Range (0, floorSize - xM);
+				zRandom = Random.Range (0, floorSize - zM);
 			}
-			else if (i < 3 + xRandom)
-				i++;
-			else {
-				i = xRandom;
-				j++;
-			}
+			else
+				break;
 		}
 		if (attempts > 4) {
 			Debug.Log ("Exceeded 5 attempts, room creation failed.");
 			return;
-		}
-		else {
-			for (int ii = xRandom; ii < 3 + xRandom; ii++) {
-				for (int jj = zRandom; jj < 3 + zRandom; jj++) {
-					m.cells [ii, floor, jj].cellType = c_type.ROOM;
+		} else {
+			for (int i = xRandom; i < xRandom + x; i++) {
+				for (int j = zRandom; j < zRandom + z; j++) {
+					m.cells [i, floor, j].cellType = c_type.ROOM;
+					m.removeWalls (i, floor, j, 4, debug);
 				}
 			}
 		}
-		*/
+
+		for (int i = xRandom; i < xRandom + x; i++) {
+			m.createWalls (i, floor, zRandom, 0, debug);
+			m.createWalls (i, floor, zRandom + zM, 2, debug);
+		}
+		for (int j = zRandom; j < zRandom + z; j++) {
+			m.createWalls (xRandom, floor, j, 1, debug);
+			m.createWalls (xRandom + xM, floor, j, 3, debug);
+		}
+
+
+
+	}
+
+	private void buildThreeByThreeRoom(int floor, bool debug = false) {
+
+		Debug.Assert (floorSize > 2);
+
+		int xRandom = Random.Range (0, floorSize - 2);
+		int zRandom = Random.Range (0, floorSize - 2);
+
 
 
 		int attempts = 0;
 		while (attempts < 5) {
 			bool clear = true;
-			if (m.cells [xRandom, floor, zRandom].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom + 1, floor, zRandom].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom + 2, floor, zRandom].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom, floor, zRandom + 1].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom + 1, floor, zRandom + 1].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom + 2, floor, zRandom + 1].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom, floor, zRandom + 2].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom + 1, floor, zRandom + 2].cellType == c_type.ROOM)
-				clear = false;
-			if (m.cells [xRandom + 2, floor, zRandom + 2].cellType == c_type.ROOM)
-				clear = false;
+
+			for (int i = xRandom; i < xRandom + 3; i++) {
+				for (int j = zRandom; j < zRandom + 3; j++) {
+					if (m.cells [i, floor, j].cellType == c_type.ROOM)
+						clear = false;
+				}
+			}
 			if (!clear) {
 				attempts++;
 				xRandom = Random.Range (0, floorSize - 2);
@@ -108,18 +121,12 @@ public class RoomBuilder : MonoBehaviour {
 			Debug.Log ("Exceeded 5 attempts, room creation failed.");
 			return;
 		} else {
-			m.cells [xRandom, floor, zRandom].cellType = c_type.ROOM;
-			m.cells [xRandom + 1, floor, zRandom].cellType = c_type.ROOM;
-			m.cells [xRandom + 2, floor, zRandom].cellType = c_type.ROOM;
-			m.cells [xRandom, floor, zRandom + 1].cellType = c_type.ROOM;
-			m.cells [xRandom + 1, floor, zRandom + 1].cellType = c_type.ROOM;
-			m.cells [xRandom + 2, floor, zRandom + 1].cellType = c_type.ROOM;
-			m.cells [xRandom, floor, zRandom + 2].cellType = c_type.ROOM;
-			m.cells [xRandom + 1, floor, zRandom + 2].cellType = c_type.ROOM;
-			m.cells [xRandom + 2, floor, zRandom + 2].cellType = c_type.ROOM;
+			for (int i = xRandom; i < xRandom + 3; i++) {
+				for (int j = zRandom; j < zRandom + 3; j++) {
+					m.cells [i, floor, j].cellType = c_type.ROOM;
+				}
+			}
 		}
-
-
 
 		m.createWalls (xRandom, floor, zRandom, 0, debug);
 		m.createWalls (xRandom, floor, zRandom, 1, debug);
@@ -144,35 +151,5 @@ public class RoomBuilder : MonoBehaviour {
 		m.removeWalls (xRandom + 2, floor, zRandom + 1, 0, debug);
 		m.removeWalls (xRandom + 2, floor, zRandom + 1, 2, debug);
 
-
-		/*
-		m.createWalls (xRandom, floor, zRandom, 0, false);
-		m.createWalls (xRandom, floor, zRandom, 1, false);
-		m.createWalls (xRandom + 1, floor, zRandom, 0, false);
-		m.createWalls (xRandom + 2, floor, zRandom, 0, false);
-		m.createWalls (xRandom + 2, floor, zRandom, 3, false);
-		m.createWalls (xRandom, floor, zRandom + 1, 1, false);
-		m.createWalls (xRandom, floor, zRandom + 2, 1, false);
-		m.createWalls (xRandom, floor, zRandom + 2, 2, false);
-		m.createWalls (xRandom + 1, floor, zRandom + 2, 2, false);
-		m.createWalls (xRandom + 2, floor, zRandom + 2, 2, false);
-		m.createWalls (xRandom + 2, floor, zRandom + 2, 3, false);
-		m.createWalls (xRandom + 2, floor, zRandom + 1, 3, false);
-
-		m.removeWalls (xRandom + 1, floor, zRandom + 1, 4, false);
-		m.removeWalls (xRandom + 1, floor, zRandom, 1, false);
-		m.removeWalls (xRandom + 1, floor, zRandom, 3, false);
-		m.removeWalls (xRandom, floor, zRandom + 1, 0, false);
-		m.removeWalls (xRandom, floor, zRandom + 1, 2, false);
-		m.removeWalls (xRandom + 1, floor, zRandom + 2, 1, false);
-		m.removeWalls (xRandom + 1, floor, zRandom + 2, 3, false);
-		m.removeWalls (xRandom + 2, floor, zRandom + 1, 0, false);
-		m.removeWalls (xRandom + 2, floor, zRandom + 1, 2, false);
-		*/
-
-	}
-
-	private void buildXByYRoom(int floor, int x, int y) {
-		
 	}
 }
